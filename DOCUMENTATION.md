1a84f5de40fa7a725d593c91ffde5ee2e44d558c
Satvik Virmani <virmanisatvik01@gmail.com>
2022-10-04 15:00:31 +0530

    Added the word-break feature

--- DOCUMENTATION.md
+++ DOCUMENTATION.md
@@ -9,6 +9,7 @@ Wrap text by a given width.
 
 - **String** `input`: The input text.
 - **Number** `width`: The wrapping width.
+- **shouldBreak** `boolean`: If the words should break or not.
 
 #### Return
 - **String** The wrapped text.
--- example/index.js
+++ example/index.js
@@ -2,30 +2,7 @@
 
 const wrapText = require("../lib");
 
-// Default width (80 chars)
-console.log(wrapText("Amet ad minima expedita dicta itaque cum debitis odio cupiditate, animi. Libero maiores quisquam consequuntur maiores nihil nobis. Cumque suscipit asperiores voluptate magnam iste suscipit reprehenderit adipisci quasi. Officia quia."));
-// =>
-// Amet ad minima expedita dicta itaque cum debitis odio cupiditate, animi. Libero
-// maiores quisquam consequuntur maiores nihil nobis. Cumque suscipit asperiores
 
-
-// 20 width
-console.log(wrapText(
-    "Lorem vitae quasi quaerat et consectetur quis ea! Eos unde repellendus soluta eaque accusamus deserunt maiores? Facere nihil architecto facilis fuga quidem? Nostrum animi consectetur quis atque architecto totam molestias."
-  , 20
-));
-// =>
-// Lorem vitae quasi
-// quaerat et
-// consectetur quis ea!
-// Eos unde repellendus
-// soluta eaque
-// accusamus deserunt
-// maiores? Facere nihil
-// architecto facilis
-// fuga quidem? Nostrum
-// animi consectetur
-// quis atque architecto
 
 // Multiline text
 console.log(wrapText(
@@ -34,6 +11,35 @@ console.log(wrapText(
 Amet reprehenderit nesciunt eius magnam doloribus porro voluptatum. Repudiandae voluptatum neque neque quos adipisci accusamus. Sunt esse incidunt aperiam voluptate id unde quasi. Ad distinctio minus ad eius perferendis autem.
 Lorem eius ullam vero sequi exercitationem maiores nam possimus. Perspiciatis tempore magnam nostrum expedita blanditiis quas laboriosam ipsam corporis. Est quasi ullam vero laborum perspiciatis eius eos aut. Libero ab.`
   , 50
+));
+// =>
+// Elit voluptate magni exercitationem voluptatem
+// consequatur inventore. Incidunt reiciendis
+// laudantium vero a voluptas quam! Quod officia
+// consectetur eaque perspiciatis modi. Cumque
+// temporibus earum mollitia deleniti autem nam
+// facilis neque tenetur?
+//
+// Amet reprehenderit nesciunt
+// eius magnam doloribus porro voluptatum. Repudiandae
+// voluptatum neque neque quos adipisci accusamus.
+// Sunt esse incidunt aperiam voluptate id unde quasi.
+// Ad distinctio minus ad eius perferendis
+// autem.
+// Lorem eius ullam vero sequi exercitationem
+// maiores nam possimus. Perspiciatis tempore magnam
+// nostrum expedita blanditiis quas laboriosam ipsam
+// corporis. Est quasi ullam vero laborum perspiciatis
+
+console.log("\n\n\n\n")
+
+// Multiline text with break
+console.log(wrapText(
+  `Elit voluptate magni exercitationem voluptatem consequatur inventore. Incidunt reiciendis laudantium vero a voluptas quam! Quod officia consectetur eaque perspiciatis modi. Cumque temporibus earum mollitia deleniti autem nam facilis neque tenetur?
+
+Amet reprehenderit nesciunt eius magnam doloribus porro voluptatum. Repudiandae voluptatum neque neque quos adipisci accusamus. Sunt esse incidunt aperiam voluptate id unde quasi. Ad distinctio minus ad eius perferendis autem.
+Lorem eius ullam vero sequi exercitationem maiores nam possimus. Perspiciatis tempore magnam nostrum expedita blanditiis quas laboriosam ipsam corporis. Est quasi ullam vero laborum perspiciatis eius eos aut. Libero ab.`
+, 50, true
 ));
 // =>
 // Elit voluptate magni exercitationem voluptatem
